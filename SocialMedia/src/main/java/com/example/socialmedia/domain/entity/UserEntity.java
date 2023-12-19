37b3e47075c7f6a9688a227db7aa27a269ada77b
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-12-19 19:33:30 +0200

    commit

---
+++ SocialMedia/src/main/java/com/example/socialmedia/domain/entity/UserEntity.java
@@ -0,0 +1,71 @@
+package com.example.socialmedia.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String picture;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public UserEntity setPicture(String picture) {
+        this.picture = picture;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
